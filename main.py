e232d97afba410e38f25c598a1fd25fe51c8a38a
MeMe4834 <127177460+MeMe4834@users.noreply.github.com>
2023-04-05 11:18:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 name = input("이름을 입력하세요: ")
 PID = input("학번을 입력하세요: ")
-Dep = input(“학번을 입력하세요: “)
+Dep = input("학번을 입력하세요: ")
+Uni = input("학교를 입력하세요: ")
+
 print("이름: "+name)
 print("학번: "+PID)
-print(“학과: “+Dep)
+print("학과: "+Dep)
+print("학교: "+Uni)
